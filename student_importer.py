a4c613defa611293e037697ae6ebd8882006e28a
Ubuntu <ubuntu@ip-172-31-3-11.us-east-2.compute.internal>
2022-05-29 00:51:08 +0000

    Fixed various student import bugs

--- student_importer.py
+++ student_importer.py
@@ -8,33 +8,35 @@ db = get_db()
 
 root = os.path.dirname(os.path.realpath(__file__))
 
-base_time = 1591624800 #remember to update
+base_time = 1654695000 #remember to update. current: wed jun 08 2022
 tdeltasec = 60 * 3
 
 grades = {8: [], 9: [], 10: [], 11: []}
 for row in csv.reader(open(root + "/students.csv")):
-	grades[int(row[1])].append(row[0])
+    grades[int(row[0])].append(row[1])
 
 students = []
 for grade in range(8, 12):
-
-    if grade == 11 or grade == 10:
-        days = 0
-    else if grades == 9  or grades == 8:
+    offset = 0
+    days = 0
+    if grade == 9  or grade == 8:
         days = 1
-    else:
-        days = 2
+    if grade == 8 or grade == 10:
+        offset = 9000    #+2.5 hours from 930 to 12
 
-	dsec = days * 3600 * 24
-	random.shuffle(grades[grade])
-	for i, student in enumerate(grades[grade]):
-		time_allowed_in = base_time + dsec + (i * tdeltasec)
-		students.append([student, time_allowed_in, i])
+    dsec = days * 3600 * 24
+    random.shuffle(grades[grade])
+    for i, student in enumerate(grades[grade]):
+        time_allowed_in = base_time + dsec + (i * tdeltasec) + offset
+        students.append([student, time_allowed_in, i])
 
 c = db.cursor()
+i = 0
 for student in students:
-	print(student)
-	c.execute("insert into students (student_id, student_username, time_allowed_in, num) values (?, NULL, ?, ?)", student)
+    i += 1
+    print(student)
+    c.execute("insert into students (student_id, student_username, time_allowed_in, num) values (?, NULL, ?, ?)", student)
 
+print(i)
 db.commit()
 db.close()
